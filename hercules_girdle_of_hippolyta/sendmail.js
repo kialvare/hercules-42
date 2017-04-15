066e0cbd87d68b96dfcf7fbbd40517e875809437
Kim <kimalvarez88@gmail.com>
2017-04-14 19:54:40 -0700

    added

--- hercules_girdle_of_hippolyta/sendmail.js
+++ hercules_girdle_of_hippolyta/sendmail.js
@@ -1,26 +1,39 @@
+'use strict';
 const nodemailer = require('nodemailer');
-// var smtpTransport = require('nodemailer-smtp-transport');
+var smtpTransport = require('nodemailer-smtp-transport');
 
-let transporter = nodemailer.createTransport({
-	service: 'gmail',
-	auth: {
-		user: 'kimalvarez88@gmail.com',
-		pass: 'jmexpiczibvkezfr'
-	}
-});
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+let transporter = nodemailer.createTransport(smtpTransport({
+	host: 'smtp.42.us.org',
+	port: 25
+	//auth: {
+		//user: 'kimalvarez88@gmail.com',
+		//pass: 'dxumebeqngcylyut'
+	//}
+}));
+
+//var img = require("fs").readFileSync("food-05.jpg");
 
 let mailOptions = {
 	from: ' "Kim Alvarez" <kimalvarez88@gmail.com>',
-	to: 'crenfrow@student.42.us.org',
-	subject: 'whoa',
-	text: 'cr3nfr0vv',
-	html: '<p>cr3nfr0vv</p>'
+	to: 'kimalvarez88@gmail.com',
+	subject: 'butts',
+	//text: 'sending this with all the smtp stuff.',
+	html: '<p>ahh</p>',
+	//attachments: [
+	//{
+	//	filename: 'food-05.jpg',
+	//	path: '~/Downloads/food-05.jpg',
+	//	contents: img,
+	//	cid: cid
+	//}
+	//] 
 };
 
 transporter.sendMail(mailOptions, (error, info) => {
 	if (error) {
 		return console.log(error);
 	}
-	console.log('Message %s sent: %s', info.messageID, info.response);
+	console.log('Message sent!');
 });
 
