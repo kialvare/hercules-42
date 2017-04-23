e701a6b635c884deae6b3373f1d7427e6784e6d5
Kim <kimalvarez88@gmail.com>
2017-04-22 19:53:11 -0700

    twerkin' on it

--- hercules_cattle_of_geryon/test.js
+++ hercules_cattle_of_geryon/test.js
@@ -1,24 +1,98 @@
 var http = require('http');
-var options = {
-	host: 'www.google.com',
-	path: '/index.html'
-};
+var request = require('request');
+var bl = require('bl');
 
-var req = http.get(options, function(res) {
-	console.log('STATUS: ' + res.statusCode);
-	console.log('HEADERS: ' + JSON.stringify(res.headers));
-		
-	// Buffer the body entirely for processing as a whole.
-	var bodyChunks = [];
-	res.on('data', function(chunk) {
-	// You can process streamed parts here...
-		 bodyChunks.push(chunk);
-	}).on('end', function() {
-		var body = Buffer.concat(bodyChunks);
-		console.log('BODY: ' + body);
-	// ...and/or process the entire body here.
-		})
-	});
-	req.on('error', function(e) {
-		console.log('ERROR: ' + e.message);
-});
+var concurrentRequests = process.argv[3]
+var requestsInProgress = 0
+var requestsSoFar = 0
+var totalRequests = 100
+var totalRequestsSoFar = 
+var maxDelay = 300
+
+for (var i = 2; i < process.argv.length; i++) {
+	var arg = process.argv[i]
+
+	var concurrent = arg.match(/-c(\d+)/)
+	var reps = arg.match(/-r(\d+)/)
+	var delay = arg.match(/-d(\d+)/)
+
+	if (concurrent) {
+		concurrentRequests = parseInt(concurrent[1])
+	}
+
+	if (reps) {
+		totalRequestsToMake = parseInt(reps[1])
+	}
+
+	if (delay) {
+		maxDelay = parseInt(delay[1])
+	}
+}
+
+if (concurrentRequests == 1) {
+	console.log('Will send', totalRequestsToMake, 'requests with', concurrentRequests,
+	'concurrent user and a maximum delay of', maxDelay)
+} else {
+	console.log('Will send', totalRequestsToMake, 'requests with', concurrentRequests,
+	'concurrent users and a maximum delay of', maxDelay)
+}
+
+
+function makeRequest() {
+	requestsSoFar += 1
+	requestsInProgress += 1
+
+	var randomDelay = Math.random() * maxDelay
+	var startTime = Date.now()
+
+	http.get(process.argv[4], function (response) {
+		response.pipe(bl(function(err, data) {
+			requestsInProgress -= 1
+				if (err)
+					return console.error(err);
+		}))
+	})
+}
+
+function start() {
+	if (requestsInProgress >= concurrentRequests) {
+		return
+	}
+
+	if (totalRequestsSoFar < totalRequestsToMake) {
+		totalRequestsSoFar += 1
+
+		request(totalRequestsSoFar)
+
+		start()
+	} else if (totalRequestsCompleted >= totalRequestsToMake) {
+		console.log('Done!')
+		//benchmarks()
+	}
+}
+
+start()
+
+//function benchmarks() {
+	//console.log("The server is now under siege...");
+	//console.log("Lifting the server siege...");
+	//console.log("Transactions: " + i); // How many times it ran
+	//console.log("Availability: "); // How available the server is
+	//console.log("Elapsed time: "); // How much time it took
+	//console.log("Data transferred: "); //done me thinks
+	//console.log("Response time: ");
+	//console.log("Transaction rate: ");
+	//console.log("Throughput: ");
+	//console.log("Concurrency: ");
+	//console.log("Successful Transactions: ");
+	//console.log("Failed Transactions: ");
+	//console.log("Longest Transactions: ");
+	//console.log("Shortest Transactions: ");
+//}
+
+//process.on('SIGINT', function() {
+//    console.log("Caught interrupt signal");
+
+//	    if (i_should_exit)
+//		        process.exit();
+//});
